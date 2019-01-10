378870fe585dcaaf0cf84d3c9ee0d20cfd241286
Dan Paulat <dpaulat@gmail.com>
2019-01-09 23:33:46 -0700

    Fixing session ended request

--- src/main/java/net/dpaulat/apps/owletnotifier/alexa/SessionEndedRequestHandler.java
+++ src/main/java/net/dpaulat/apps/owletnotifier/alexa/SessionEndedRequestHandler.java
@@ -2,8 +2,8 @@ package net.dpaulat.apps.owletnotifier.alexa;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
-import com.amazon.ask.model.LaunchRequest;
 import com.amazon.ask.model.Response;
+import com.amazon.ask.model.SessionEndedRequest;
 import com.amazon.ask.request.Predicates;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +13,7 @@ import java.util.Optional;
 public class SessionEndedRequestHandler implements RequestHandler {
     @Override
     public boolean canHandle(HandlerInput handlerInput) {
-        return handlerInput.matches(Predicates.requestType(LaunchRequest.class));
+        return handlerInput.matches(Predicates.requestType(SessionEndedRequest.class));
     }
 
     @Override
