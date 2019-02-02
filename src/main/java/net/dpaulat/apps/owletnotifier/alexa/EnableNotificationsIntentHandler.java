f4d2391f46a0f4a4fa3bf2f26e66979dd04aed07
Dan Paulat <dpaulat@gmail.com>
2019-02-01 20:53:55 -0700

    Ask for permission to create reminders

--- src/main/java/net/dpaulat/apps/owletnotifier/alexa/EnableNotificationsIntentHandler.java
+++ src/main/java/net/dpaulat/apps/owletnotifier/alexa/EnableNotificationsIntentHandler.java
@@ -2,6 +2,7 @@ package net.dpaulat.apps.owletnotifier.alexa;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.Permissions;
 import com.amazon.ask.model.Response;
 import com.amazon.ask.request.Predicates;
 import net.dpaulat.apps.owlet.OwletApi;
@@ -9,6 +10,8 @@ import net.dpaulat.apps.owletnotifier.ConfigProperties;
 import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -29,6 +32,24 @@ public class EnableNotificationsIntentHandler implements RequestHandler {
 
     @Override
     public Optional<Response> handle(HandlerInput handlerInput) {
+
+        Permissions permissions = handlerInput
+                .getRequestEnvelope()
+                .getContext()
+                .getSystem()
+                .getUser()
+                .getPermissions();
+
+        if (permissions == null || permissions.getConsentToken() == null) {
+            String speechText = "In order to enable notifications, this skill needs permission to create reminders.  Please grant permission using the card sent to your Alexa app.";
+            List<String> list = new ArrayList<>();
+            list.add("alexa::alerts:reminders:skill:readwrite");
+            return handlerInput.getResponseBuilder()
+                    .withSpeech(speechText)
+                    .withAskForPermissionsConsentCard(list)
+                    .build();
+        }
+
         String speechText = "OK, notifications are enabled on this device using reminders.";
 
         return handlerInput.getResponseBuilder()
